7b1571241b9d7ccf6756fe334d8726b1766c8efa
Eike Mueller <e.mueller@bath.ac.uk>
2022-05-06 17:21:32 +0100

    Some refactoring.
    Anchor boxes should be passed to the constructor, and the bounding boxes should only encode the centre, width, height and class id. The create_target method is moved to a new class for preparing the data for the model.

--- coco_image_reader.py
+++ coco_image_reader.py
@@ -1,10 +1,8 @@
-import json
 from pycocotools.coco import COCO
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-from anchor_boxes import BestAnchorBoxFinder
 
 
 class COCOImageReader(object):
@@ -40,17 +38,15 @@ class COCOImageReader(object):
         self.coco = COCO(annotations_file)
 
         categories = self.coco.loadCats(self.coco.getCatIds())
-        self.category_name_map = {x["id"]: x["name"] for x in categories}
-        self.category_idx_map = {x["id"]: j for j, x in enumerate(categories)}
-        self.n_classes = len(self.category_idx_map)
+        # Map from category ids to classes (=class indices)
+        self.category_class_map = {x["id"]: j for j, x in enumerate(categories)}
+        # Map from classes (=class indices) to category names
+        self.class_category_name_map = {j: x["name"] for j, x in enumerate(categories)}
+        self.n_classes = len(self.category_class_map)
         print(f"number of classes = {self.n_classes}")
         self.all_category_names = set([category["name"] for category in categories])
         if verbose:
             print("COCO categories: \n{}\n".format(", ".join(self.all_category_names)))
-        with open("anchor_boxes.json", "r", encoding="utf8") as f:
-            self.anchors = json.load(f)
-        self.n_anchor = len(self.anchors)
-        self.babf = BestAnchorBoxFinder(self.anchors)
 
     def get_image_ids(self, category_names=None):
         """Return the ids of all images in the dataset which belong to a certain category
@@ -73,15 +69,15 @@ class COCOImageReader(object):
         Returns a dictionary of the form {'image':(CV2 image object),'bboxes'(bbox annotations)}
 
         The bounding box annotations consist of a list of dictionaries of the following form:
-        { 'category_id':   the id of the labelled object category according to COCO
-          'category_name': the name of the category for the labelled object
-          'class':         the class index, which is a number in the range 0,1,...,n_cat-1
-                           where n_cat is the number of categories in the dataset
-          'xc':            the x-coordinate of the center of the bbox
+        { 'xc':            the x-coordinate of the center of the bbox
           'yc':            the y-coordinate of the center of the bbox
           'width':         the width of the bounding box
           'height':        the height of the bounding box
+          'class':         the class index, which is a number in the range 0,1,...,n_cat-1
+                           where n_cat is the number of categories in the dataset
         }
+
+        All bounding box coordinates are given in absolute values (i.e. not scaled by image size)
 
         :arg img_id: id of image to process
         """
@@ -94,14 +90,11 @@ class COCOImageReader(object):
         annotations = self.coco.loadAnns(ann_ids)
         bboxes = []
         for annotation in annotations:
-            label = self.category_name_map[annotation["category_id"]]
-            idx = self.category_idx_map[annotation["category_id"]]
+            class_id = self.category_class_map[annotation["category_id"]]
             xmin, ymin, width, height = annotation["bbox"]
             bboxes.append(
                 {
-                    "category_id": annotation["category_id"],
-                    "class": idx,
-                    "category_name": label,
+                    "class": class_id,
                     "xc": round(self.image_size / img["width"] * (xmin + 0.5 * width)),
                     "yc": round(
                         self.image_size / img["height"] * (ymin + 0.5 * height)
@@ -111,70 +104,6 @@ class COCOImageReader(object):
                 }
             )
         return {"image": image, "bboxes": bboxes}
-
-    def create_target(self, bboxes):
-        """Create a target for list of bounding boxes (obtained with read_image)
-
-        The target y is an array of shape (n_tiles,n_tiles,n_anchor,5+n_classes)
-        where
-          * n_tiles is the number of tiles the image is subdivided into
-          * n_anchor is the number of anchor boxes
-          * n_classes is the number of classes
-
-        A bounding box is associated with a tile if the centre of the bounding box lies in that tile
-
-        Let i=(i_x,i_y) be the index of the tile and j be the anchor box. Then we have that:
-
-        xc and yc are measured relative to the corner of the tile that contains the center
-        of the object and they are normalised to the tile size, i.e. xc, yc in [0,1].
-
-          xc_{i,j} := y(i_x,i_y,j,0) is the x- coordinate of the center of the
-                                       bounding box paired with the j-th anchor
-          yc_{i,j} := y(i_x,i_y,j,1) is the x- coordinate of the center
-          w_{i,j} := y(i_x,i_y,j,2) is the width of the bounding box
-          h_{i,j} := y(i_x,i_y,j,3) is the height of the bounding box
-          C_{i,j} := y(i_x,i_y,j,4) = 1 if the j-the anchor box contains an object and
-                                        zero otherwise
-          p_{i,j,k} := y(i_x,i_y,j,5+k) = 1 if the object in the box has class k and
-                                            zero otherwise
-
-        :arg bbox: bounding box annotations, in the format generated by the read_image()
-                   method
-        """
-        # Size of tiles in the image
-        tile_size = self.image_size // self.n_tiles
-        tiled_annotations = {}
-        for bbox in bboxes:
-            # Work out the tile that contains the bbox centre
-            tile_x = bbox["xc"] // tile_size
-            tile_y = bbox["yc"] // tile_size
-            tile_id = (tile_x, tile_y)
-            if tile_id in tiled_annotations.keys():
-                tiled_annotations[tile_id].append(bbox)
-            else:
-                tiled_annotations[tile_id] = [
-                    bbox,
-                ]
-        # Now populate the target tensor y
-        y_target = np.zeros(
-            (self.n_tiles, self.n_tiles, self.n_anchor, 5 + self.n_classes)
-        )
-        for tile_id, bboxes in tiled_annotations.items():
-            tile_x, tile_y = tile_id
-            # Match bounding box to the anchor with the best overlap
-            # j = anchor box index
-            # k = bounding box index
-            anchor_ind, bbox_ind = self.babf.match(bboxes)
-            for j, k in zip(anchor_ind, bbox_ind):
-                xc = bboxes[k]["xc"] / tile_size - tile_x
-                yc = bboxes[k]["yc"] / tile_size - tile_y
-                y_target[tile_x, tile_y, j, 0] = xc
-                y_target[tile_x, tile_y, j, 1] = yc
-                y_target[tile_x, tile_y, j, 2] = bboxes[k]["width"] / self.image_size
-                y_target[tile_x, tile_y, j, 3] = bboxes[k]["height"] / self.image_size
-                y_target[tile_x, tile_y, j, 4] = 1
-                y_target[tile_x, tile_y, j, 5 + bboxes[k]["class"]] = 1
-        return y_target
 
     def show_annotated_image(self, annotated_image):
         """Auxilliary function for displaying an annotated image generated by the read_image()
@@ -204,7 +133,7 @@ class COCOImageReader(object):
             plt.text(
                 bbox["xc"] - 0.5 * bbox["width"],
                 bbox["yc"] - 0.5 * bbox["height"],
-                bbox["category_name"],
+                self.class_category_name_map[bbox["class"]],
                 color="yellow",
             )
         ax.set_xticks(self.image_size / self.n_tiles * np.arange(self.n_tiles))
